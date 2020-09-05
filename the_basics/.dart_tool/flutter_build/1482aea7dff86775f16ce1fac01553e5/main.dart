4f7f8d771bd19e93416ba4af08a4ad056b2483c7
thecbkm <rjoshi14899@gmail.com>
2020-09-05 11:34:58 +0530

    f1

---
+++ the_basics/.dart_tool/flutter_build/1482aea7dff86775f16ce1fac01553e5/main.dart
@@ -0,0 +1,10 @@
+import 'dart:ui' as ui;
+
+import 'package:the_basics/main.dart' as entrypoint;
+
+Future<void> main() async {
+  if (true) {
+    await ui.webOnlyInitializePlatform();
+  }
+  entrypoint.main();
+}
